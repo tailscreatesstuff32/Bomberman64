b336038fc6930b12ae6d4b6e9a963c3096e3ccf5
tailsluver29 <66333352+tailscreatesstuff32@users.noreply.github.com>
2022-12-11 20:00:38 -0800

    Add files via upload

---
+++ ASM/entry.asm
@@ -0,0 +1,29 @@
+arch n64.cpu
+endian msb
+
+//output "Bomberman64U.z64",create
+output "BM64U.z64",create
+fill $00101000 
+ 
+origin $00000000
+base $A4000000
+
+
+include "HEADER\HEADER_BM64.asm"
+
+include "BOOT\boot.asm"
+
+
+//insert "BOOT\BIN\BOOTCODE_BM64.BIN"
+
+ 
+base $80000400
+
+
+entry_point:
+
+//how entry point get to this address?
+L80000400:
+
+j L80000400
+nop
