587d68cc949c9fbcfc86f5337da1036deba4a5b0
tailsluver29 <66333352+tailscreatesstuff32@users.noreply.github.com>
2022-12-11 22:00:38 -0800

    Add files via upload

--- ASM/entry.asm
+++ ASM/entry.asm
@@ -3,7 +3,7 @@ endian msb
 
 //output "Bomberman64U.z64",create
 output "BM64U.z64",create
-//fill $00800000
+fill $00800000
 
 
  
@@ -23,9 +23,31 @@ base $80000400
 
 
 entry_point:
+lui t0,$8002
+addiu t0,t0,-$4040
+ori t1,r0,$c4a0
 
-//how entry point get to this address?
-L80000400:
-
-j L80000400
+L8000040C:
+addi t1,t1,-$8
+sw $0, $0(t0)
+sw $40,$4(t0)
+bne t1,$0,$8000040c
+addi t0,t0,$8
+lui t2,$8000
+lui sp,$8002
+addiu t2,t2,$19a0
+jr t2
+addiu sp,sp,$44b0
 nop
+nop
+nop
+nop
+nop
+nop
+nop
+//j L80000400
+j L8000040C
+nop
+
+
+
